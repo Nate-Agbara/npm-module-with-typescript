6eb9c2c88e6cdb66db055ec0b7e4eb9700124b93
Nate-Agbara <Agbaraabah1>
2022-04-07 23:22:46 +0100

    first commit

---
+++ build/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var sum = function (a, b) { return a + b; };
+// console.log(sum(2,3))
+
+exports.sum = sum;
+//# sourceMappingURL=index.js.map
